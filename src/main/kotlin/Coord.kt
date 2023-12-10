7bb6853f57681516c3ae7ce3fbede357de077ccf
Tobi <mail@glueckstobi.de>
2023-12-10 18:25:22 +0100

    day 10, part 1

--- src/main/kotlin/Coord.kt
+++ src/main/kotlin/Coord.kt
@@ -1,1 +1,21 @@
-data class Coord(val x: Int, val y: Int)
+data class Coord(val x: Int, val y: Int) {
+    inline fun forEachAdjacent(block: (coord: Coord) -> Unit) {
+        (x - 1..x + 1).forEach { i ->
+            block(Coord(i, y - 1))
+            block(Coord(i, y + 1))
+        }
+        block(Coord(x - 1, y))
+        block(Coord(x + 1, y))
+    }
+}
+
+
+enum class Direction(val dx: Int, val dy: Int) {
+    N(0, -1),
+    S(0, 1),
+    W(-1, 0),
+    E(1, 0);
+
+    fun go(source: Coord): Coord = Coord(source.x + dx, source.y+dy)
+
+}
